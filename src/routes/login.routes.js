975fd74cdd7f39dc21c7fe7719ad9d610efedf10
Ernesto151 <ernestoriera2001@gmail.com>
2024-11-22 16:05:17 +0100

    ruta fecha

--- src/routes/login.routes.js
+++ src/routes/login.routes.js
@@ -7,4 +7,9 @@ const router= Router();
 // Ruta para autenticación
 router.post("/login", autenticarUsuario);
 
+router.get("/fecha", async (req, res)=>{
+    const result= await pool.query('SELECT NOW()');
+    return res.json(result.rows[0]);
+})
+
 export default router;
