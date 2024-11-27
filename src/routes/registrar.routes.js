10f52c06eb04ea8fcd57b4751a0041a329ddd20a
Ernesto151 <ernestoriera2001@gmail.com>
2024-11-27 05:33:51 +0100

    rectificando

--- src/routes/registrar.routes.js
+++ src/routes/registrar.routes.js
@@ -15,16 +15,15 @@ router.post("/auth", async (req, res) => {
         const query = `SELECT rol FROM Usuarios WHERE usuario = $1 AND contrasena = $2`;
         const values = [user, password];
         
-        await pool.query(query, values);
+        const result = await pool.query(query, values);
 
-        if (!row) {
+        if (result.rows.length === 0) {
             // Usuario o contraseña incorrectos
             return res.status(401).send("Credenciales incorrectas.");
         }
 
         // Devolver el rol del usuario
-        return res.status(200).json({ rol: row.rol });
-
+        return res.status(200).json({ rol: result.rows[0].rol });
     } catch (error) {
         console.error("Error al autenticar:", error.message);
         return res.status(500).send("Error en el servidor.");
