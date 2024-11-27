3ec6d75e12cf119a361cfe419f9c363c2c0e283d
Ernesto151 <ernestoriera2001@gmail.com>
2024-11-27 04:45:44 +0100

    limpiando

--- src/db.js
+++ src/db.js
@@ -9,21 +9,7 @@ export const pool= new pg.Pool({
     password:'LE1WViqlxw2z',
     database:'datos',
     port:"5432",
-    //connectionString: process.env.DATABASE_URL,
     ssl: {
         rejectUnauthorized: false 
     }
 });
-
-// pool.query(`
-// CREATE TABLE Permisos (
-//     id SERIAL PRIMARY KEY,
-//     usuario_id INTEGER,
-//     documento_acta_id INTEGER,
-//     documento_exp_id INTEGER,
-//     tipo_documento TEXT,
-//     FOREIGN KEY (usuario_id) REFERENCES Usuarios(id) ON DELETE CASCADE,
-//     FOREIGN KEY (documento_acta_id) REFERENCES DocumentosActas(id) ON DELETE CASCADE,
-//     FOREIGN KEY (documento_exp_id) REFERENCES DocumentosExp(id) ON DELETE CASCADE
-// );
-// `)
